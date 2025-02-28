388a58628da10b463db04fccb45ff6db7451eaee
Evitha <72492491+eviitamin@users.noreply.github.com>
2025-02-28 19:23:32 +0800

    Add files via upload

---
+++ Registration Form/script.js
@@ -0,0 +1,339 @@
+var subjectObject = {
+    "NCR": {
+    "Caloocan":['Barangay','Barangay','Barangay'],
+
+    "Las Pinas":['Barangay','Barangay','Barangay'],
+
+    "Makati":['Barangay','Barangay','Barangay'],
+
+    "Malabon":['Barangay','Barangay','Barangay'],
+
+    "Mandaluyong":['Barangay','Barangay','Barangay'],
+
+    "Manila":['Binondo','Ermita','Intramuros','Malate','Paco(Dilao',
+    'Pandacan','Port Area','Quiapo','Sampaloc','San Miguel','San Nicolas',
+    'Santa Ana','Santa Cruz','Tondo'],
+
+    "Marikina":['Barangay','Barangay','Barangay'],
+
+    "Muntinlupa":['Barangay','Barangay','Barangay'],
+
+    "Navatos":['Barangay','Barangay','Barangay'],
+
+    "Paranaque":['Barangay','Barangay','Barangay'],
+
+    "Pasay":['Barangay','Barangay','Barangay'],
+
+    "Pasig":['Barangay','Barangay','Barangay'],
+
+    "Quezon City":['Barangay','Barangay','Barangay'],
+
+    "San Juan":['Barangay','Barangay','Barangay'],
+
+    "Taguig":['Barangay','Barangay','Barangay'],
+
+    "Valenzuela":['Barangay','Barangay','Barangay']
+
+    },
+    "Region I - Ilocos": {
+        "Ilocos Norte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Ilocos Sur": ['Barangay','Barangay','Barangay','Barangay',],
+        "La Union": ['Barangay','Barangay','Barangay','Barangay',],
+        "Pangasinan": ['Barangay','Barangay','Barangay','Barangay',]
+    },
+    "Region II - Cagayan Valley": {
+        "Batanes": ['Barangay','Barangay','Barangay','Barangay',],
+        "Cagayan": ['Barangay','Barangay','Barangay','Barangay',],
+        "Isabela": ['Barangay','Barangay','Barangay','Barangay',],
+        "Nueva Vizcaya": ['Barangay','Barangay','Barangay','Barangay',],
+        "Quirino": ['Barangay','Barangay','Barangay','Barangay',]
+    },
+    "Region III - Central Luzon": {
+        "Aurora": ['Barangay','Barangay','Barangay','Barangay',],
+        "Bataan": ['Barangay','Barangay','Barangay','Barangay',],
+        "Bulacan": ['Barangay','Barangay','Barangay','Barangay',],
+        "Nueva Ecija": ['Barangay','Barangay','Barangay','Barangay',],
+        "Pampanga": ['Barangay','Barangay','Barangay','Barangay',],
+        "Tarlac": ['Barangay','Barangay','Barangay','Barangay',],
+        "Zambales": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Angeles": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Olongapo": ['Barangay','Barangay','Barangay','Barangay',]
+    },
+    "Region IV-A - CALABARZON": {
+        "Batangas": ['Barangay','Barangay','Barangay','Barangay',],
+        "Cavite": ['Barangay','Barangay','Barangay','Barangay',],
+        "Laguna": ['Barangay','Barangay','Barangay','Barangay',],
+        "Quezon": ['Barangay','Barangay','Barangay','Barangay',],
+        "Rizal": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Lucena": ['Barangay','Barangay','Barangay','Barangay',]
+    },
+    "MIMAROPA": {
+        "Marinduque": ['Barangay','Barangay','Barangay','Barangay',],
+        "Occidental Mindoro": ['Barangay','Barangay','Barangay','Barangay',],
+        "Oriental Mindoro": ['Barangay','Barangay','Barangay','Barangay',],
+        "Palawan": ['Barangay','Barangay','Barangay','Barangay',],
+        "Romblon": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Puerto Princesa": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region V - Bicol": {
+        "Albay": ['Barangay','Barangay','Barangay','Barangay',],
+        "Camarines Norte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Camarines Sur": ['Barangay','Barangay','Barangay','Barangay',],
+        "Cataduanes": ['Barangay','Barangay','Barangay','Barangay',],
+        "Masbate": ['Barangay','Barangay','Barangay','Barangay',],
+        "Sorsogon": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region VI - Western Visayas": {
+        "Aklan": ['Barangay','Barangay','Barangay','Barangay',],
+        "Antique": ['Barangay','Barangay','Barangay','Barangay',],
+        "Capiz": ['Barangay','Barangay','Barangay','Barangay',],
+        "Guimaras": ['Barangay','Barangay','Barangay','Barangay',],
+        "Iloilo": ['Barangay','Barangay','Barangay','Barangay',],
+        "Negros Occidental": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Bacolod": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Iloilo": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region VII - Central Visayas": {
+        "Bohol": ['Barangay','Barangay','Barangay','Barangay',],
+        "Cebu": ['Barangay','Barangay','Barangay','Barangay',],
+        "Negros Oriental": ['Barangay','Barangay','Barangay','Barangay',],
+        "Siquijor": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Cebu": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Lapu-Lapu": ['Barangay','Barangay','Barangay','Barangay',],
+        "City or Mandaue": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region VIII - Eastern Visayas": {
+        "Biliran": ['Barangay','Barangay','Barangay','Barangay',],
+        "Eastern Samar": ['Barangay','Barangay','Barangay','Barangay',],
+        "Leyte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Northern Samar": ['Barangay','Barangay','Barangay','Barangay',],
+        "Samar": ['Barangay','Barangay','Barangay','Barangay',],
+        "Southern Leyte": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Tacloban": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region IX - Zamboanga Peninsula": {
+        "Zamboanga del Norte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Zamboanga del Sur": ['Barangay','Barangay','Barangay','Barangay',],
+        "Zamboanga Sibugay": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Isabela": ['Barangay','Barangay','Barangay','Barangay',],
+        "Zamboanga City": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region X - Northern Mindanao": {
+        "Bukidnon": ['Barangay','Barangay','Barangay','Barangay',],
+        "Camiguin": ['Barangay','Barangay','Barangay','Barangay',],
+        "Lanao del Norte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Misamis Occidental": ['Barangay','Barangay','Barangay','Barangay',],
+        "Misamis Oriental": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Cagayan de Oro": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Iligan": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region XI - Davao": {
+        "Davao de Oro (Compostela Valley)": ['Barangay','Barangay','Barangay','Barangay',],
+        "Davao del Norte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Davao del Sur": ['Barangay','Barangay','Barangay','Barangay',],
+        "Davao Occidental": ['Barangay','Barangay','Barangay','Barangay',],
+        "Davao Oriental": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Davao": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region XII - SOCCSKARGEN": {
+        "Cotabato (North Cotabato)": ['Barangay','Barangay','Barangay','Barangay',],
+        "Sarangani": ['Barangay','Barangay','Barangay','Barangay',],
+        "South Cotabato": ['Barangay','Barangay','Barangay','Barangay',],
+        "Sultan Kudarat": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Cotabato": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of General Santos": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "Region XIII - Caraga": {
+        "Agusan del Norte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Agusan del Sur": ['Barangay','Barangay','Barangay','Barangay',],
+        "Dinagat Islands": ['Barangay','Barangay','Barangay','Barangay',],
+        "Surigao del Norte": ['Barangay','Barangay','Barangay','Barangay',],
+        "Surigao del Sur": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Butuan": ['Barangay','Barangay','Barangay','Barangay',],
+    },
+    "CAR": {
+        "Abra": ['Barangay','Barangay','Barangay','Barangay',],
+        "Apayao": ['Barangay','Barangay','Barangay','Barangay',],
+        "Benguet": ['Barangay','Barangay','Barangay','Barangay',],
+        "Igufao": ['Barangay','Barangay','Barangay','Barangay',],
+        "Kalinga": ['Barangay','Barangay','Barangay','Barangay',],
+        "Mountain Province": ['Barangay','Barangay','Barangay','Barangay',],
+        "City of Baguio": ['Abanao-Zandueta-Kayong-Chugum',
+        'Alfonso Tabora',
+        'Ambiong',
+        'Andres Bonifacio',
+       'Apugan-Loakan',
+        'Asin Road',
+        'Atok Trail',
+        'Aurora Hill Proper',
+        'Aurora Hill, North Central',
+        'Aurora Hill, South Central',
+        'Bagong Lipunan (Market Area)',
+        'Bakakeng Central',
+        'Bakakeng North',
+        'Bal-Marcoville (Marcoville)',
+        'Balsigan',
+        'Bayan Park East',
+        'Bayan Park Village',
+        'Bayan Park West (Bayan Park)',
+        'BGH Compound',
+        'Bonifacio-Caguioa-Rimando',
+        'Brookside',
+        'Brookspoint',
+        'Cabinet Hill-Teacher’s Camp',
+        'Camdas Subdivision',
+        'Camp 7',
+        'Camp 8',
+        'Camp Allen',
+        'Campo Filipino',
+        'City Camp Central',
+        'City Camp Proper',
+        'Country Club Village',
+        'Cresencia Village Barangay',
+        'Dagsian, Lower',
+        'Dagsian, Upper',
+        'Department of Public Services (DPS) Compound',
+        'Dizon Subdivision',
+        'Dominican Hill Mirador',
+        'Dontogan',
+        'Engineers Hill',
+        'Fairview Village',
+        'Ferdinand (Happy Homes-Campo)',
+        'Fort del Pilar',
+        'Gabriela Silang',
+        'General Emilio F. Aguinaldo',
+        'General Luna, Lower',
+        'General Luna, Upper',
+        'Gibraltar',
+        'Greenwater Village',
+        'Guisad Central',
+        'Guisad Sorong',
+        'Happy Hollow',
+        'Happy Homes (Happy Homes-Lucban)',
+        'Harrison-Claudio Carantes',
+        'Hillside',
+        'Holy Ghost Extension',
+        'Holy Ghost Proper',
+        'Honeymoon (Honeymoon-Holy Ghost)',
+        'Imelda R. Marcos (La Salle)',
+        'Imelda Village',
+        'Irisan',
+        'Kabayanihan',
+        'Kagitingan',
+        'Kayang Extension',
+        'Kayang-Hilltop',
+        'Kias',
+        'Legarda-Burnham-Kisad',
+        'Liwanag-Loakan',
+        'Loakan Proper',
+        'Lopez Jaena',
+        'Lourdes Subdivision Extension',
+        'Lourdes Subdivision, Lower',
+        'Lourdes Subdivision, Proper',
+        'Lualhati',
+        'Lucnab',
+        'Magsaysay Private Road',
+        'Magsaysay, Lower',
+        'Magsaysay, Upper',
+        'Malcolm Square-Perfecto',
+        'Manuel A. Roxas',
+        'Market Subdivision, Upper',
+        'Middle Quezon Hill Subdivision',
+        'Military Cut-off',
+        'Mines View Park',
+        'Modern Site, East',
+        'Modern Site, West',
+        'MRR-Queen Of Peace',
+        'New Lucban',
+        'Outlook Drive',
+        'Pacdal',
+        'Padre Burgos',
+        'Padre Zamora',
+        'Palma-Urbano',
+        'Phil-Am',
+        'Pinget',
+        'Pinsao Pilot Project',
+        'Pinsao Proper',
+        'Pucsusan',
+        'Puliwes',
+        'Quezon Hill Proper',
+        'Quezon Hill, Upper',
+        'Quirino Hill, East',
+        'Quirino Hill, Lower',
+        'Quirino Hill, Middle',
+        'Quirino Hill, West',
+        'Quirino-Magsaysay, Upper',
+        'Rizal Monument Area',
+        'Rock Quarry, Lower',
+        'Rock Quarry, Middle',
+        'Rock Quarry, Upper',
+        'Saint Joseph Village',
+        'Salud Mitra',
+        'San Antonio Village',
+        'San Luis Village',
+        'San Roque Village',
+        'San Vicente',
+        'Sanitary Camp South',
+        'Sanitary Camp, North',
+        'Santa Escolastica',
+        'Santo Rosario Valley',
+        'Santo Tomas Proper',
+        'Santo Tomas School Area',
+        'Scout Barrio',
+        'Session Road Area',
+        'Slaughter House Area',
+        'SLU-SVP Housing Village',
+        'South Drive',
+        'Teodora Alonzo',
+        'Trancoville',
+        'Victoria Village']
+    },
+    "ARMM": {
+        "Basilan": ['Barangay','Barangay','Barangay','Barangay',],
+        "Lanao del Sur": ['Barangay','Barangay','Barangay','Barangay',],
+        "Maguindanao": ['Barangay','Barangay','Barangay','Barangay',],
+        "Sulu": ['Barangay','Barangay','Barangay','Barangay',],
+        "Tawi-tawi": ['Barangay','Barangay','Barangay','Barangay',],
+    }
+}
+
+window.onload = function(){
+    var first = document.getElementById('first')
+    var second = document.getElementById('second')
+    var third = document.getElementById('third')
+    var fourth = document.getElementById('third-add')
+    var fifth = document.getElementById('third-label')
+
+    for(x in subjectObject){
+    first.options[first.options.length] = new Option(x)}
+
+    first.onchange = function(){
+        second.length = 1
+        third.length = 1
+        fourth.length = 1
+
+        second.style.display = 'block'
+        third.style.display = 'none'
+        fourth.style.display = 'none'
+
+        for (var y in subjectObject[this.value]){
+            second.options[second.options.length] = new Option(y)
+        }
+    }
+
+    second.onchange = function(){
+        third.length = 1
+        fourth.length = 1
+
+        third.style.display = 'block'
+        fourth.style.display = 'block'
+        fifth.style.display = 'block'
+        z = subjectObject[first.value][this.value]
+        console.log(z);
+        for(let i=0; i<z.length; i++){
+            third.options[third.options.length] = new Option(z[i])
+        }
+    }
+
+}
+
